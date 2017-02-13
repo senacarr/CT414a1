87e3512916348eff3bfdee699ca89496e68e3ab5
Paul Meaney <p.meaney3@nuigalway.ie>
2017-02-13 23:10:47 +0000

    softchanges

--- CT414assignment1/test/bank/TransactionTest.java
+++ CT414assignment1/test/bank/TransactionTest.java
@@ -6,7 +6,7 @@ public class TransactionTest {
 
 	@Test
 	public void test() {
-		Account testacc = new Account("Test", "TestPassword", 1001, 100, 696969);
+		Account testacc = new Account("Test", "TestPassword", 1001, 100);
 		testacc.newTransaction("Withdrawal",10);
 		testacc.newTransaction("Deposit", 20);
 		Statement testStatement = new Statement(testacc, "12/02/2017", "15/02/2017" );
