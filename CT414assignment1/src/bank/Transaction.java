18cae4d4ffe939870276839378ae8bc6b906d767
Paul Meaney <p.meaney3@nuigalway.ie>
2017-02-13 13:13:29 +0000

    added transaction

--- CT414assignment1/src/bank/Transaction.java
+++ CT414assignment1/src/bank/Transaction.java
@@ -1,5 +1,45 @@
 package bank;
 
 public class Transaction {
+	
+	private String timestamp;
+    private String transactionType;
+    private int amount;
+    
+    public Transaction(String timestamp, String transactionType, int amount){
+    	this.timestamp = timestamp;
+    	this.transactionType = transactionType;
+    	this.amount = amount;
+    }
 
-}
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+    
+    
+    
+    
+    
+
+
+}   
