7d185e61416dc0e5975142d0b96e82a9966b5281
senacarr <senansenanc@gmail.com>
2017-02-13 13:05:27 +0000

    add project files, account class

---
+++ CT414assignment1/src/bank/Account.java
@@ -0,0 +1,68 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+	
+	private String name;
+	private String password;
+	private int accountNum;
+	private int amount;
+	private long seshID;
+	private List<Transaction> transactions;
+	
+	
+	public Account(String uName, String uPassword, int uAccountNum, int uAmount, long uSeshID) {
+		this.name         = uName;
+		this.password     = uPassword;
+		this.accountNum   = uAccountNum;
+		this.amount       = uAmount;
+		this.seshID       = uSeshID;
+		this.transactions = new ArrayList<Transaction>();
+	}
+
+	public String getAccountName() {
+		return name;
+	}
+	
+	public String getAccountPassword() {
+		return password;
+	}
+	
+	public int getAccountNum() {
+		return accountNum;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public long getSessionID() {
+		return seshID;
+	}
+	
+	public void newTransaction(int timeStamp, String tType, int tAmount) {
+		// transactions.add(new Transaction(timeStamp, tType, tAmount));
+	}
+	
+	public List<Transaction> getTransactions() {
+		return transactions == null ? new ArrayList<Transaction>() : transactions;
+	}
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
